77bc3118ba40ce76290c744a44433d82e96ca4e5
Filip H.F. "FiXato" Slagter <fixato+github+plexodustools+jekylltakeout@gmail.com>
2020-12-03 04:27:25 +0100

    Ensure the _data/posts folder exists

--- import_json_data_file.sh
+++ import_json_data_file.sh
@@ -2,6 +2,7 @@
 input="$1"
 filename="$(basename "$input")"
 clean_filename="$(printf '%s' "$filename" | sed 's/ /_/g;s/[^a-zA-Z0-9_\.)(-\[\]]*//g')"
+mkdir -p "_data/posts"
 
 if hash ditto 2>/dev/null; then
   CP_COMMAND='ditto'
